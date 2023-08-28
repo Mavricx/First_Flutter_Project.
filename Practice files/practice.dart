8e5cce3cd184833e70b60b337be4e18d63276547
Priyanshu Pattanayak <ppattanayakcodes@gmail.com>
2023-08-28 16:18:02 +0530

    updated log in page

--- Practice files/practice.dart
+++ Practice files/practice.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 // ignore: depend_on_referenced_packages
 import 'package:intl/intl.dart';
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Log in '),
     );
   }
 }
@@ -45,18 +46,19 @@ class _MyHomePageState extends State<MyHomePage> {
   var emailText = TextEditingController();
   var password = TextEditingController();
   var time = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.greenAccent,
         title: Text(widget.title),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           const Text(
-            "LOG-IN PAGE",
+            "LOG-IN ",
             style: TextStyle(
                 fontSize: 25,
                 fontFamily: "bungee",
@@ -110,7 +112,23 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Text(
                 "Log In",
                 style: TextStyle(fontSize: 20),
-              ))
+              )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print("Password forgotten");
+                  },
+                  child: Text("Password Forgotten ?")),
+              ElevatedButton(
+                  onPressed: () {
+                    print("Create new Account");
+                  },
+                  child: Text("Create New Account")),
+            ],
+          ),
+          Text("*Terms & Conditions.",style: TextStyle(fontSize: 15),)
         ],
       ),
     );
